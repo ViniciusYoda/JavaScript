ac38b8c81562d58df0865196640ebec99d139bde
Vinicius <vyoda4604@gmail.com>
2025-05-27 17:01:57 -0300

    Code editors

---
+++ info/1-The JavaScript language/1-An introduction/1-3-Code editors.js
@@ -0,0 +1,48 @@
+// Editores de código
+
+// Um editor de código é o lugar onde os programadores passam a maior parte do tempo.
+
+// Existem dois tipos principais de editores de código: IDEs e editores leves. Muitas pessoas usam uma ferramenta de cada tipo.
+// IDE em Inglês
+
+// O termo IDE (Integrated Development Environment) refere-se a um editor poderoso com muitos recursos que geralmente opera em um “projeto inteiro”. Como o nome sugere, não é apenas um editor, mas um “ambiente de desenvolvimento” em grande escala.
+
+// Um IDE carrega o projeto (que pode ser muitos arquivos), permite a navegação entre arquivos, fornece preenchimento automático com base em todo o projeto (não apenas o arquivo aberto) e se integra a um sistema de gerenciamento de versão (como git), um ambiente de teste e outras coisas de “nível de projeto”.
+
+// Se você ainda não selecionou um IDE, considere as seguintes opções:
+
+//     Código do Visual Studio (cross-plataforma, grátis).
+//     WebStorm (cross-platform, pago).
+
+// Para o Windows, há também o “Visual Studio”, para não ser confundido com “Visual Studio Code”. “Visual Studio” é um editor pago e poderoso, adequado para o Windows. Plataforma da NET. Também é bom em JavaScript. Há também uma versão gratuita do Visual Studio Community.
+
+// Muitos IDEs são pagos, mas têm um período de avaliação. Seu custo é geralmente insignificante em comparação com o salário de um desenvolvedor qualificado, então basta escolher o melhor para você.
+// Editores leves
+
+// Os “editores leves” não são tão poderosos quanto os IDEs, mas são rápidos, elegantes e simples.
+
+// Eles são usados principalmente para abrir e editar um arquivo instantaneamente.
+
+// A principal diferença entre um “editor leve” e um “IDE” é que um IDE funciona em um nível de projeto, por isso carrega muito mais dados no início, analisa a estrutura do projeto, se necessário e assim por diante. Um editor leve é muito mais rápido se precisarmos de apenas um arquivo.
+
+// Na prática, os editores leves podem ter muitos plugins, incluindo analisadores de sintaxe de nível de diretório e autocompletadores, portanto, não há uma fronteira rígida entre um editor leve e um IDE.
+
+// Existem muitas opções, por exemplo:
+
+//     Texto Sublime (cross-plataforma, shareware).
+//     Notepad++ (Windows, grátis).
+//     Vim e Emacs também são legais se você souber como usá-los.
+
+// Não vamos discutir
+
+// Os editores nas listas acima são aqueles que eu ou meus amigos que considero bons desenvolvedores têm usado há muito tempo e estamos felizes.
+
+// Há outros grandes editores em nosso grande mundo. Por favor, escolha o que você mais gosta.
+
+// A escolha de um editor, como qualquer outra ferramenta, é individual e depende de seus projetos, hábitos e preferências pessoais.
+
+// Opinião pessoal do autor:
+
+//     Eu usaria o Visual Studio Code se eu desenvolver principalmente frontend.
+//     Caso contrário, se for principalmente outra linguagem/plataforma e parcialmente frontend, considere outros editores, como XCode (Mac), Visual Studio (Windows) ou família Jetbrains (Webstorm, PHPStorm, RubyMine etc., dependendo do idioma).
+
