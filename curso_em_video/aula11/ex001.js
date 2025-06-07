b9eae84c9b9b684352d086eab16600cea0f4cf97
Vinícius Yoda <vyoda4604@gmail.com>
2025-06-07 13:20:09 -0300

    condições

---
+++ curso_em_video/aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60) {
+    console.log(`Você ultrapssou a velocidade permitidae`)
+} 
+console.log(`Dirige sempre usando cinto de segurança`)
